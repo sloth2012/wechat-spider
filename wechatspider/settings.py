b9102f6aaaf21fe9c5a22a24d17a16cb57f6b3cf
liuxiang <lxmtlab@gmail.com>
2018-03-01 17:20:47 +0800

    work version

--- wechatspider/settings.py
+++ wechatspider/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'gzid5#%@yqg_4=mh_8)*zpe%9z*&vap20=h$sg7hi@u-ljyw=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
